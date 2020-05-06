07b9f456abdaad7d495811e62567343862e59671
stacklast <softwareywebsoluciones@gmail.com>
2020-05-06 16:23:39 -0500

    Probando service Worker eventos y ciclo

--- sw.js
+++ sw.js
@@ -2,11 +2,26 @@ console.log('Service Worker archivo');
 
 //instalando service worker
 self.addEventListener('install', event=>{
+
+    const instalacion = new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log('SW: Instalaciones Terminadas');
+            resolve();
+        },1000);
+    });
     //descargar assets
     //cache
     console.log('instalando SW');
     console.log(event);
 
+    //carga instantanea de los cambios
+    //recomendar no usarlo
+    //self.skypWaiting();
+
+    event.waitUntil(
+        instalacion
+
+    );
 });
 
 //cuando el service worker se activa y toma el control de la aplicaci'on.
@@ -16,3 +31,8 @@ self.addEventListener('activate', event=>{
 
     console.log(' SW activo');
 })
+
+
+self.addEventListener('fetch', event =>{
+    console.log('SW:',event.request.url);
+});
