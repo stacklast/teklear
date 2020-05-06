4135be8cc9c23e121e4fb7c29854405b382c49df
stacklast <softwareywebsoluciones@gmail.com>
2020-05-06 17:45:56 -0500

    Probando service Worker eventos y ciclo

--- sw.js
+++ sw.js
@@ -36,6 +36,16 @@ self.addEventListener('activate', event=>{
 
 self.addEventListener('fetch', event =>{
 
+    const offlineResp = new Response(`
+    Bienvenido a mi pagina web
+
+    Disculpa pero para usarla necesitas Internet
+    `);
+
+    const resp = fetch(event.request)
+    .catch(()=>{
+        return offlineResp;
+    });
     //aplicar estrategias de cache
     console.log('SW:',event.request.url);
 
