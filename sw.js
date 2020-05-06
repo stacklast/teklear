566b38ee9393836e42b0e2b4c1181576f089d960
stacklast <softwareywebsoluciones@gmail.com>
2020-05-06 15:20:12 -0500

    Probando service Worker PWAS inicial

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
 console.log('Service Worker archivo');
+
+
+self.addEventListener('install', event=>{
+    //descargar assets
+    //cache
+    console.log('instalando SW');
+    console.log(event);
+
+});
